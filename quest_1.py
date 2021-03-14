eb48247acb6710f4f35e1c9fefc268cfbe472540
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-15 01:04:53 +0300

    add quest_1.py

---
+++ quest_1.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+name_file, name, time, reward = argv
+rate = 10
+try:
+    time = float(time)
+    reward = float(reward)
+    print(f"Сотрудник {name} получит зарплату: {rate * time + reward} руб.")
+except ValueError:
+    print("Параметры заданы не числом")
